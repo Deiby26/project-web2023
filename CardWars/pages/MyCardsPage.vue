68b3b729e57aca0b763396fa567f67e306cb3e7b
Emmanuel Velez <emmanuel.velezme@gmail.com>
2023-10-05 13:14:43 -0500

    fix at refresh

--- CardWars/pages/MyCardsPage.vue
+++ CardWars/pages/MyCardsPage.vue
@@ -1,102 +1,104 @@
 <template>
-    <div>
-      <h1>Mis Cartas</h1>
-      
-      <!-- Botón para abrir el diálogo -->
-      <nuxt-link to="/CardPage">Crear nueva carta</nuxt-link>
-  
-      <!-- Diálogo de edición -->
-      <div class="edit-dialog" v-if="isEditDialogOpen">
-        <h2>Editar Carta</h2>
-        <textarea v-model="editedCardContent"></textarea>
-        <input type="color" v-model="editedCardColor" />
-        <select v-model="editedCardFont">
-          <option v-for="font in availableFonts" :key="font" :value="font">{{ font }}</option>
-        </select>
-        <button @click="saveEditedCard">Guardar</button>
-        <button @click="closeEditDialog">Cancelar</button>
-      </div>
-  
-      <!-- Mostrar cartas aquí -->
-      <div v-for="(card, index) in savedCards" :key="index" class="card" :style="{ backgroundColor: card.backgroundColor, fontFamily: card.font }">
-        {{ card.content }}
-        <button @click="openEditDialog(index)">Editar</button>
-        <button @click="deleteCard(index)">Eliminar</button>
-      </div>
+  <div>
+    <h1>Mis Cartas</h1>
+
+    <!-- Botón para abrir el diálogo -->
+    <nuxt-link to="/CardPage">Crear nueva carta</nuxt-link>
+
+    <!-- Diálogo de edición -->
+    <div class="edit-dialog" v-if="isEditDialogOpen">
+      <h2>Editar Carta</h2>
+      <textarea v-model="editedCardContent"></textarea>
+      <input type="color" v-model="editedCardColor" />
+      <select v-model="editedCardFont">
+        <option v-for="font in availableFonts" :key="font" :value="font">{{ font }}</option>
+      </select>
+      <button @click="saveEditedCard">Guardar</button>
+      <button @click="closeEditDialog">Cancelar</button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        savedCards: [],
-        isEditDialogOpen: false,
-        editedCardContent: '',
-        editedCardColor: '',
-        editedCardFont: '',
-        editedCardIndex: null,
-        availableFonts: ['Arial, sans-serif', 'Times New Roman', 'Courier New']
-      };
-    },
-    created() {
+
+    <!-- Mostrar cartas aquí -->
+    <div v-for="(card, index) in savedCards" :key="index" class="card"
+      :style="{ backgroundColor: card.backgroundColor, fontFamily: card.font }">
+      {{ card.content }}
+      <button @click="openEditDialog(index)">Editar</button>
+      <button @click="deleteCard(index)">Eliminar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      savedCards: [],
+      isEditDialogOpen: false,
+      editedCardContent: '',
+      editedCardColor: '',
+      editedCardFont: '',
+      editedCardIndex: null,
+      availableFonts: ['Arial, sans-serif', 'Times New Roman', 'Courier New']
+    };
+  },
+  created() {
+    if (typeof localStorage !== 'undefined') {
       this.savedCards = JSON.parse(localStorage.getItem('savedCards')) || [];
+    }
+  },
+  methods: {
+    deleteCard(index) {
+      this.savedCards.splice(index, 1);
+      if (typeof localStorage !== 'undefined') {
+        localStorage.setItem('savedCards', JSON.stringify(this.savedCards));
+      }
     },
-    methods: {
-      deleteCard(index) {
-        this.savedCards.splice(index, 1);
+    openEditDialog(index) {
+      this.isEditDialogOpen = true;
+      this.editedCardContent = this.savedCards[index].content;
+      this.editedCardColor = this.savedCards[index].backgroundColor || '#ffffff';
+      this.editedCardFont = this.savedCards[index].font || 'Arial, sans-serif';
+      this.editedCardIndex = index;
+    },
+    closeEditDialog() {
+      this.isEditDialogOpen = false;
+      this.editedCardContent = '';
+      this.editedCardColor = '';
+      this.editedCardFont = '';
+      this.editedCardIndex = null;
+    },
+    saveEditedCard() {
+      if (this.editedCardIndex !== null) {
+        this.savedCards[this.editedCardIndex].content = this.editedCardContent;
+        this.savedCards[this.editedCardIndex].backgroundColor = this.editedCardColor;
+        this.savedCards[this.editedCardIndex].font = this.editedCardFont;
         if (typeof localStorage !== 'undefined') {
           localStorage.setItem('savedCards', JSON.stringify(this.savedCards));
         }
-      },
-      openEditDialog(index) {
-        this.isEditDialogOpen = true;
-        this.editedCardContent = this.savedCards[index].content;
-        this.editedCardColor = this.savedCards[index].backgroundColor || '#ffffff';
-        this.editedCardFont = this.savedCards[index].font || 'Arial, sans-serif';
-        this.editedCardIndex = index;
-      },
-      closeEditDialog() {
-        this.isEditDialogOpen = false;
-        this.editedCardContent = '';
-        this.editedCardColor = '';
-        this.editedCardFont = '';
-        this.editedCardIndex = null;
-      },
-      saveEditedCard() {
-        if (this.editedCardIndex !== null) {
-          this.savedCards[this.editedCardIndex].content = this.editedCardContent;
-          this.savedCards[this.editedCardIndex].backgroundColor = this.editedCardColor;
-          this.savedCards[this.editedCardIndex].font = this.editedCardFont;
-          if (typeof localStorage !== 'undefined') {
-            localStorage.setItem('savedCards', JSON.stringify(this.savedCards));
-          }
-          this.closeEditDialog();
-        }
+        this.closeEditDialog();
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .card {
-    display: inline-block;
-    padding: 20px;
-    border: 1px solid #ccc;
-    margin-top: 20px;
-    margin-right: 10px;
   }
-  
-  .edit-dialog {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #fff;
-    padding: 20px;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    z-index: 9999;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.card {
+  display: inline-block;
+  padding: 20px;
+  border: 1px solid #ccc;
+  margin-top: 20px;
+  margin-right: 10px;
+}
+
+.edit-dialog {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 20px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 9999;
+}
+</style>
