8c19097ca50e591f402269119e28b48aa7892b25
Deiby26 <davidbohorquez150@gmail.com>
2023-10-12 17:00:51 -0500

    add: login and singup complete

--- CardWars/pages/Login.vue
+++ CardWars/pages/Login.vue
@@ -2,15 +2,15 @@
   <center>
     <div class="container">
       <div class="col-2">
-        <form>
+        <form @submit="submitlogin">
           <br>
           <p id="Username">USERNAME</p>
-          <input type="text" placeholder="Your Username here" v-model="username" required>
+          <input type="text" placeholder="Your Username here" v-model="usernamel" required>
           <p id="Email-Address">EMAIL  ADDRESS</p>
-          <input type="email" placeholder="Your Email here" v-model="email" required>
+          <input type="email" placeholder="Your Email here" v-model="emaill" required>
           <p id="Password">PASSWORD</p>
-          <input type="password" placeholder="Password here" v-model="password" required>
-          <button type="submit" id="Login">Login</button>
+          <input type="password" placeholder="Password here" v-model="passwordl" required>
+          <button type="submit">Login</button>
         </form>
       </div>
     </div>
@@ -19,17 +19,83 @@
 
 <script >
 
-
+const contenedor = []; // Declarar la variable local aquí
 export default {
   data() {
     return {
-      username: '',
-      email: '',
-      password: ''
+      usernamel: '',
+      emaill: '',
+      passwordl: ''
     };
   },
+  
+  methods: {
+  submitlogin(event) {
+    event.preventDefault();
+    const contenedor = []; // Declarar la variable contenedor como una lista vacía
+
+    var objetoRecuperado = JSON.parse(localStorage.getItem('JSON'));
+    const nombrelogin = this.usernamel;
+    const nombrelocalstorage = objetoRecuperado["username"];
+
+    const sonIgualesnombre = nombrelogin === nombrelocalstorage;
+    if (sonIgualesnombre == true){
+      contenedor.push(true);
+    } else {
+      contenedor.push(false);
+    }
+
+    const correol = this.emaill;
+    const correolocalstorage = objetoRecuperado["email"];
+
+    const sonIgualescorreo = correol === correolocalstorage;
+    if (sonIgualescorreo == true){
+      contenedor.push(true);
+    } else {
+      contenedor.push(false);
+    }
+    //const contraseñal = this.usernamel;
+    //const correolocalstorage = objetoRecuperado["email"];
+    const contral = this.passwordl;
+    const passwordlocalstorage = objetoRecuperado["password"];
+    const sonIgualescontra = contral === passwordlocalstorage;
+
+    if (sonIgualescontra == true){
+      contenedor.push(true);
+    } else {
+      contenedor.push(false);
+    }
+
+
+
+    console.log(contenedor)
+    var contador = 0
+    for (var i = 0; i < contenedor.length; i++) {
+      var elemento = contenedor[i];
+      if (elemento === true) {
+        contador++;
+      }else{
+        continue
+      }
+    }
+    if (contador === 3){
+      window.alert("INCIO DE SECCION")
+    }else{
+      window.alert("LAS CREDECIALES NO COINCIDEN")
+      
+    }
+  }
+  
+}
+
+
+
 };
-var objetoRecuperado = JSON.parse(localStorage.getItem('JSON'));
+//console.log(contenedor)
+//var objetoRecuperado = JSON.parse(localStorage.getItem('JSON'));
+//console.log(objetoRecuperado)
+//console.log(objetoRecuperado["username"]);
+
 
 
 
