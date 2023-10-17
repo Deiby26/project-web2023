adc784039ddb7627dd7448ae7dc98fb5b925cfcd
Deiby26 <davidbohorquez150@gmail.com>
2023-10-17 16:47:12 -0500

    Update: background login / sing up

--- CardWars/pages/Login.vue
+++ CardWars/pages/Login.vue
@@ -1,126 +1,133 @@
-<template>
-  <center>
-    <div class="container">
-      <div class="col-2">
-        <form @submit="submitlogin">
-          <br>
-          <p id="Username">USERNAME</p>
-          <input type="text" placeholder="Your Username here" v-model="usernamel" required>
-          <p id="Email-Address">EMAIL  ADDRESS</p>
-          <input type="email" placeholder="Your Email here" v-model="emaill" required>
-          <p id="Password">PASSWORD</p>
-          <input type="password" placeholder="Password here" v-model="passwordl" required>
-          <button type="submit">Login</button>
-        </form>
+  <template>
+    <center>
+      <div class="container">
+        <div class="col-2">
+          <div class="styled-div">
+            <br>
+            <form @submit="submitlogin">
+              <br>
+              <p id="Username">USERNAME</p>
+              <input type="text" placeholder="Your Username here" v-model="usernamel" required>
+              <p id="Email-Address">EMAIL ADDRESS</p>
+              <input type="email" placeholder="Your Email here" v-model="emaill" required>
+              <p id="Password">PASSWORD</p>
+              <input type="password" placeholder="Password here" v-model="passwordl" required>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </center>
-</template>
+    </center>
+  </template>
 
-<script >
+  <style>
 
-const contenedor = []; // Declarar la variable local aquí
-export default {
-  data() {
-    return {
-      usernamel: '',
-      emaill: '',
-      passwordl: ''
-    };
-  },
-  
-  methods: {
-  submitlogin(event) {
-    event.preventDefault();
-    const contenedor = []; // Declarar la variable contenedor como una lista vacía
-
-    var objetoRecuperado = JSON.parse(localStorage.getItem('JSON'));
-    const nombrelogin = this.usernamel;
-    const nombrelocalstorage = objetoRecuperado["username"];
-
-    const sonIgualesnombre = nombrelogin === nombrelocalstorage;
-    if (sonIgualesnombre == true){
-      contenedor.push(true);
-    } else {
-      contenedor.push(false);
-    }
-
-    const correol = this.emaill;
-    const correolocalstorage = objetoRecuperado["email"];
-
-    const sonIgualescorreo = correol === correolocalstorage;
-    if (sonIgualescorreo == true){
-      contenedor.push(true);
-    } else {
-      contenedor.push(false);
-    }
-    //const contraseñal = this.usernamel;
-    //const correolocalstorage = objetoRecuperado["email"];
-    const contral = this.passwordl;
-    const passwordlocalstorage = objetoRecuperado["password"];
-    const sonIgualescontra = contral === passwordlocalstorage;
-
-    if (sonIgualescontra == true){
-      contenedor.push(true);
-    } else {
-      contenedor.push(false);
-    }
+  .styled-div {
+    background-color: rgba(233, 38, 200, 0.5);
+    width: 600px; 
+    height: 350px; 
+    border-radius: 20px;
+  }
+  </style>
 
 
 
-    console.log(contenedor)
-    var contador = 0
-    for (var i = 0; i < contenedor.length; i++) {
-      var elemento = contenedor[i];
-      if (elemento === true) {
-        contador++;
+
+  <script >
+
+  const contenedor = []; 
+  export default {
+    data() {
+      return {
+        usernamel: '',
+        emaill: '',
+        passwordl: ''
+      };
+    },
+    
+    methods: {
+    submitlogin(event) {
+      event.preventDefault();
+      const contenedor = []; 
+
+      var objetoRecuperado = JSON.parse(localStorage.getItem('JSON'));
+      const nombrelogin = this.usernamel;
+      const nombrelocalstorage = objetoRecuperado["username"];
+
+      const sonIgualesnombre = nombrelogin === nombrelocalstorage;
+      if (sonIgualesnombre == true){
+        contenedor.push(true);
+      } else {
+        contenedor.push(false);
+      }
+
+      const correol = this.emaill;
+      const correolocalstorage = objetoRecuperado["email"];
+
+      const sonIgualescorreo = correol === correolocalstorage;
+      if (sonIgualescorreo == true){
+        contenedor.push(true);
+      } else {
+        contenedor.push(false);
+      }
+
+      const contral = this.passwordl;
+      const passwordlocalstorage = objetoRecuperado["password"];
+      const sonIgualescontra = contral === passwordlocalstorage;
+
+      if (sonIgualescontra == true){
+        contenedor.push(true);
+      } else {
+        contenedor.push(false);
+      }
+
+
+
+      console.log(contenedor)
+      var contador = 0
+      for (var i = 0; i < contenedor.length; i++) {
+        var elemento = contenedor[i];
+        if (elemento === true) {
+          contador++;
+        }else{
+          continue
+        }
+      }
+      if (contador === 3){
+        window.alert("INCIO DE SECCION")
+        this.$router.push('/initialized');
       }else{
-        continue
+        window.alert("LAS CREDECIALES NO COINCIDEN")
+        
       }
     }
-    if (contador === 3){
-      window.alert("INCIO DE SECCION")
-      this.$router.push('/initialized');
-    }else{
-      window.alert("LAS CREDECIALES NO COINCIDEN")
-      
-    }
+    
   }
-  
-}
 
 
 
-};
-//console.log(contenedor)
-//var objetoRecuperado = JSON.parse(localStorage.getItem('JSON'));
-//console.log(objetoRecuperado)
-//console.log(objetoRecuperado["username"]);
+  };
 
+  </script>
 
+  <style>
+  #Username{
+    font-family: sans-serif;
+    color: white;
+  }
 
+  #Email-Address{
+    font-family: sans-serif;
+    color: white;
+  }
 
+  #Password{
+    font-family: sans-serif;
+    color: white;
+  }
 
-</script>
-
-<style>
-#Username{
-  font-family: sans-serif;
-  color: white;
-}
-
-#Email-Address{
-  font-family: sans-serif;
-  color: white;
-}
-
-#Password{
-  font-family: sans-serif;
-  color: white;
-}
-
-#Login{
-  font-family: sans-serif;
-  color: white;
-}
-</style>
+  #Login{
+    font-family: sans-serif;
+    color: white;
+  }
+  </style>
--- CardWars/pages/SignUp.vue
+++ CardWars/pages/SignUp.vue
@@ -1,68 +1,81 @@
-<template>
-  <center>
-    <div class="container">
-      <div class="col-2">
-        <form @submit="submitForm">
-          <br>
-          <p style="color: aliceblue;">USERNAME</p>
-          <input type="text" placeholder="Create your Username here" v-model="username" required>
-          <p style="color: aliceblue;">EMAIL ADDRESS</p>
-          <input type="email" placeholder="Type your Email here" v-model="email" required>
-          <p style="color: aliceblue;" >PASSWORD</p>
-          <input type="password" placeholder="Create your Password here" v-model="password" required>
-          <p style="color: aliceblue;" >PASSWORD CONFIRM</p>
-          <input type="password" placeholder="Confirm your Password here" v-model="password2" required>
-          <input type="checkbox" id="cbox2" value="second_checkbox" required/>
-          <label for="cbox2" style="color: aliceblue;">Terminos y condiciones </label>
-          
-          <nuxt-link to="/terms" style="color: aliceblue;"><p>Tratamiento de datos personales</p></nuxt-link>
-          <br>
-          <button type="submit">Sing up</button>
-          <br>
-          
-        </form>
+  <template>
+    <center>
+      <br>
+      <div class="transparent-div">
+        <div class="container">
+          <div class="col-2">
+            <form @submit="submitForm">
+              <br>
+              <p style="color: aliceblue;">USERNAME</p>
+              <input type="text" placeholder="Create your Username here" v-model="username" required>
+              <p style="color: aliceblue;">EMAIL ADDRESS</p>
+              <input type="email" placeholder="Type your Email here" v-model="email" required>
+              <p style="color: aliceblue;">PASSWORD</p>
+              <input type="password" placeholder="Create your Password here" v-model="password" required>
+              <p style="color: aliceblue;">PASSWORD CONFIRM</p>
+              <input type="password" placeholder="Confirm your Password here" v-model="password2" required>
+              <input type="checkbox" id="cbox2" value="second_checkbox" required/>
+              <label for="cbox2" style="color: aliceblue;">Terminos y condiciones </label>
+              <nuxt-link to="/terms" style="color: aliceblue;"><p>Tratamiento de datos personales</p></nuxt-link>
+              <br>
+              <button type="submit">Sign up</button>
+              <br>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </center>
-</template>
+    </center>
+  </template>
 
-<script>
+  <style>
+
+  .transparent-div {
+    background-color: rgba(197, 197, 231, 0.5);
+    width: 600px; 
+    height: 500px; 
+    border-radius: 20px;
+  } 
+  </style>
+
+
+  <script>
 
 
 
-export default {
-  data() {
-    return {
-      username: '',
-      email: '',
-      password: '',
-      password2: ''
-    };
-  },
-  methods: {
-    submitForm(event) {
-      event.preventDefault();
- 
-      const passwordCredential = this.password;
-      const passwordCredential2 = this.password2;
+  export default {
+    data() {
+      return {
+        username: '',
+        email: '',
+        password: '',
+        password2: ''
+      };
+    },
+    methods: {
+      
+      submitForm(event) {
+        event.preventDefault();
+  
+        const passwordCredential = this.password;
+        const passwordCredential2 = this.password2;
 
-      var json ={};
-      const passwordsAreEqual = passwordCredential === passwordCredential2;
-      if (passwordsAreEqual) {
-        json["username"]  = this.username;
-        json["email"] = this.email;
-        json["password"] = this.password;
-        localStorage.setItem('JSON', JSON.stringify(json));
-        this.$router.push('/Login');
-        
-      } else { 
-        window.alert("passwords are incorrect")
+        var json ={};
+        const passwordsAreEqual = passwordCredential === passwordCredential2;
+        if (passwordsAreEqual) {
+          json["username"]  = this.username;
+          json["email"] = this.email;
+          json["password"] = this.password;
+          localStorage.setItem('JSON', JSON.stringify(json));
+          this.$router.push('/Login');
+          
+        } else { 
+          window.alert("passwords are incorrect")
+        }
+
       }
-
     }
-  }
-};
+  };
 
 
 
-</script>
+  </script>
